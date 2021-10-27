51100f05e021cc1502b8ee3240233209db20b377
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2021-10-27 16:36:30 +0900

    Update install_ros2_foxy_rp4.sh

--- install_ros2_foxy_rp4.sh
+++ install_ros2_foxy_rp4.sh
@@ -41,7 +41,7 @@ sudo sh -c "rosdep init"
 rosdep update
 
 echo "[Create a catkin Workspace and fetch the core packages]"
-mkdir ~/ros_colcon_ws/src
+mkdir -p ~/ros_colcon_ws/src
 cd ~/ros_colcon_ws
 rosinstall_generator ros_base nav_msgs cv_bridge example_interfaces demo_nodes_cpp demo_nodes_py joint_state_publisher --rosdistro foxy --deps --wet-only --tar > foxy-ros_base-wet.rosinstall
 vcs import src < foxy-ros_base-wet.rosinstall
